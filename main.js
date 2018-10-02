078807e912ab080eb4f8365e1d88187952791b2c
Eugenie Ordonneau <eugenie@ordonneau.com>
2018-10-02 16:09:43 +0100

    grid is kind of working but still issues with box size

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-for(var i = 0; i < 16; i++) {
-    for(var j = 0; j < 16; j++){
-    var container = document.querySelector(".container");
-    var div = document.createElement("div");
-    div.className = "row " + j + " column " + i;
-    container.appendChild(div);
+const container = document.querySelector(".container");
+
+console.log(container);
+
+for(var i = 1; i <= 16; i++) {
+    const newColumn = document.createElement("div");
+    container.appendChild(newColumn);
+    newColumn.classList.add("column_" + i);
+    newColumn.setAttribute('style', 'display: flex; flex-direction: row; border: solid;');
+    for(var j = 1; j <= 16; j++){
+        const newRow = document.createElement("div");
+        newRow.classList.add("row_"+ j);
+        newColumn.appendChild(newRow);
+        newRow.setAttribute('style', 'display: flex; flex-direction: column;');
     }
+     
 }
