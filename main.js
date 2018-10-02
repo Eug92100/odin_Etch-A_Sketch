a9eb7bad7f357525f1b9c2c4c897af575a33b4fd
Eugenie Ordonneau <eugenie@ordonneau.com>
2018-10-02 15:01:43 +0100

    created 16*16 divs with each a specific class

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-var container = document.querySelector(".container");
-var div = document.createElement("div");
-
 for(var i = 0; i < 16; i++) {
+    for(var j = 0; j < 16; j++){
+    var container = document.querySelector(".container");
+    var div = document.createElement("div");
+    div.className = "row " + j + " column " + i;
     container.appendChild(div);
+    }
 }
