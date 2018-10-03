568202e9c14721f04395473f58a68a1af043778e
Eugenie Ordonneau <eugenie@ordonneau.com>
2018-10-03 16:44:41 +0100

    button restarts the grids but doesn't change the size of divs

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
 const container = document.querySelector(".container");
 
-
-for(var i = 1; i <= 16; i++) {
-    const newColumn = document.createElement("div");
-    container.appendChild(newColumn);
-    newColumn.classList.add("column");
-    for(var j = 1; j <= 16; j++){
-        const newRow = document.createElement("div");
-        newRow.classList.add("cell");
-        newColumn.appendChild(newRow);
-    }    
+function gridCreation(parameter){
+    for(var i = 1; i <= parameter; i++) {
+        const newColumn = document.createElement("div");
+        container.appendChild(newColumn);
+        newColumn.classList.add("column");
+        //newColumn.setAttribute('style','height: (100 / parameter)%')
+        for(var j = 1; j <= parameter; j++){
+            const newRow = document.createElement("div");
+            newRow.classList.add("cell");
+            newColumn.appendChild(newRow);
+        }    
+    }
 }
+
+
+
+var button = document.querySelector("button");
+function restart(){
+    var nb = prompt("Choose the number of small squares!");
+    cells.forEach((cell) => cell.setAttribute('style','background-color: white;'));
+    gridCreation(nb);
+}
+
+gridCreation(30);
 
 var cells = Array.from(document.querySelectorAll(".cell"));
 cells.forEach(function(cell) { 
-    addEventListener('mouseover', function(e){
-    e.target.setAttribute('style','background-color: blue;');
+    cell.addEventListener('mouseover', function(e){
+        e.target.setAttribute('style','background-color: blue;');
 })})
+
+button.addEventListener('click',restart);
