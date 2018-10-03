5107831e5977f74f61abc5760c38853c42d82025
Eugenie Ordonneau <eugenie@ordonneau.com>
2018-10-03 11:40:15 +0100

    the grid spread all over the page as expected.

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 const container = document.querySelector(".container");
+container.setAttribute("style","min-width: 400px;")
 
 console.log(container);
 
 for(var i = 1; i <= 16; i++) {
     const newColumn = document.createElement("div");
     container.appendChild(newColumn);
-    newColumn.classList.add("column_" + i);
-    newColumn.setAttribute('style', 'display: flex; flex-direction: row; border: solid;');
+    newColumn.classList.add("column");
+    newColumn.setAttribute('style', 'display: flex; flex-direction: row; height: 6.25%;');
     for(var j = 1; j <= 16; j++){
         const newRow = document.createElement("div");
-        newRow.classList.add("row_"+ j);
+        newRow.classList.add("cell");
         newColumn.appendChild(newRow);
-        newRow.setAttribute('style', 'display: flex; flex-direction: column;');
+        newRow.setAttribute('style', 'display: flex; flex-direction: column; border: 1px solid blue; width: 6.25%;');
     }
      
 }
+
