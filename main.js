05ea21f6b21cf74f1fe98dc16f73af59ce371608
Eugenie Ordonneau <eugenie@ordonneau.com>
2018-10-04 17:30:49 +0100

    The restart button is working tks gridRemove and creating hovering func

--- main.js
+++ main.js
@@ -1,34 +1,46 @@
 const container = document.querySelector(".container");
+var button = document.querySelector("button");
+
 
 function gridCreation(parameter){
     for(var i = 1; i <= parameter; i++) {
-        const newColumn = document.createElement("div");
-        container.appendChild(newColumn);
-        newColumn.classList.add("column");
-        //newColumn.setAttribute('style','height: (100 / parameter)%')
+        const newRow = document.createElement("div");
+        container.appendChild(newRow);
+        newRow.classList.add("row");
         for(var j = 1; j <= parameter; j++){
-            const newRow = document.createElement("div");
-            newRow.classList.add("cell");
-            newColumn.appendChild(newRow);
+            const newCell = document.createElement("div");
+            newCell.classList.add("cell");
+            newRow.appendChild(newCell);
         }    
     }
 }
 
-
-
-var button = document.querySelector("button");
-function restart(){
-    var nb = prompt("Choose the number of small squares!");
-    cells.forEach((cell) => cell.setAttribute('style','background-color: white;'));
-    gridCreation(nb);
+function gridRemove() {
+    var row = document.querySelector(".row");
+    while(row != null){
+        console.log(row);
+        container.removeChild(row);
+        row = document.querySelector(".row");  
+}
 }
 
-gridCreation(30);
+function hovering(){
+    var cells = Array.from(document.querySelectorAll(".cell"));
+    cells.forEach(function(cell) { 
+        cell.addEventListener('mouseover', function(e){
+            e.target.setAttribute('style','background-color: blue;');
+        });})
+}
 
-var cells = Array.from(document.querySelectorAll(".cell"));
-cells.forEach(function(cell) { 
-    cell.addEventListener('mouseover', function(e){
-        e.target.setAttribute('style','background-color: blue;');
-})})
 
+function restart(){
+    var nb = prompt("Choose the number of small squares!");
+    gridRemove();
+    gridCreation(nb);
+    hovering();
+    }
+
+gridCreation(16);
+hovering();
 button.addEventListener('click',restart);
+
